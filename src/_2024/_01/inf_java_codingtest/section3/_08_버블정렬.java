aed69c1de02bb4fe20554f393c1e6327c9670e2a
leb001122 <leb001122@icloud.com>
2024-01-15 22:51:20 +0900

    Bubble Sort

---
+++ src/_2024/_01/inf_java_codingtest/section3/_08_버블정렬.java
@@ -0,0 +1,41 @@
+package _2024._01.inf_java_codingtest.section3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _08_버블정렬 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] res = bubbleSort(arr, N);
+        for (int i = 0; i < N; i++) {
+            System.out.print(res[i] + " ");
+        }
+    }
+
+    private static int[] bubbleSort(int[] arr, final int N) {
+        for (int last = N-1; last >= 1; last--) {
+            for (int i = 0; i < last; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    swap(arr, i, i + 1);
+                }
+            }
+        }
+        return arr;
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+     }
+}
