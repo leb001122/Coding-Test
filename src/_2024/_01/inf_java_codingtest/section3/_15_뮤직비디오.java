c5a86d679f5f4cf9d25807e784384b204c2270ea
leb001122 <leb001122@icloud.com>
2024-01-25 02:16:04 +0900

    binary search

--- src/_2024/_01/inf_java_codingtest/section3/_15_뮤직비디오.java
+++ src/_2024/_01/inf_java_codingtest/section3/_15_뮤직비디오.java
@@ -20,68 +20,48 @@ public class _15_뮤직비디오 {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        int res = solution(N, M, arr);
+        System.out.println(solution(N, M, arr));
     }
 
     private static int solution(int N, int M, int[] arr) {
-        int[] sumArr = new int[M];
-        int[] startIdxArr = new int[M];
+        int answer = 0;
+        int left = arr[0]; // max value
+        int right = arr[0]; // sum
 
-        init(N, M, arr, sumArr, startIdxArr);
-        if (M == 1) {
-            return sumArr[0];
+        for (int i = 1; i < N; i++) {
+            if (left < arr[i]) left = arr[i];
+            right += arr[i];
         }
 
-        int result;
-        int maxIdx;
+        while (left <= right) {
+            int mid = (left + right) / 2;
 
-        while (true) {
-            maxIdx = 0;
-            for (int i = 1; i < M; i++) {
-                if (sumArr[maxIdx] < sumArr[i]) {
-                    maxIdx = i;
-                }
+            if (count(N, M, mid, arr) > M) { // 불만족
+                left = mid + 1;
+            } else {                         // 만족
+                right = mid - 1;
+                answer = mid;
             }
-
-            if (startIdxArr[maxIdx+1] - startIdxArr[maxIdx] == 1 || N - startIdxArr[maxIdx] == 1) {
-                result = sumArr[maxIdx];
-                break;
-            }
-
-            if (maxIdx == 0) {
-                int temp = arr[startIdxArr[maxIdx + 1] - 1];
-                sumArr[maxIdx] -= temp;
-                sumArr[maxIdx + 1] += temp;
-                startIdxArr[maxIdx + 1] -= 1;
-
-            } else if (maxIdx == N - 1) {
-                int temp = arr[startIdxArr[maxIdx]];
-                sumArr[maxIdx] -= temp;
-                sumArr[maxIdx - 1] += temp;
-                startIdxArr[maxIdx] += 1;
-
-            }else {
-
-                int temp = Math.min(arr[startIdxArr[maxIdx]], arr[startIdxArr[maxIdx + 1] - 1]);
-
-            }
-
         }
-
-        return result;
+        return answer;
     }
 
-    private static void init(int N, int M, int[] arr, int[] sumArr, int[] startIdxArr) {
+    private static int count(int N, int M, int mid, final int[] arr) {
+        int count = 1;
+        int sum = 0;
 
-        int i = 0, j = 0;
-        for (;i < M; i++) {
-            startIdxArr[i] = j;
-            for (int k = 0; k < N/M; k++) {
-                sumArr[i] += arr[j++];
+        for (int item : arr) {
+            if (sum + item > mid) {
+                count++;
+                sum = item;
+            } else {
+                sum += item;
+            }
+
+            if (count > M) {
+                break;
             }
         }
-        while (j < N) {
-            sumArr[i-1] += arr[j++];
-        }
+        return count;
     }
 }
